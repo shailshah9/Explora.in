d71fcd52e4c788c0d0ae1cd23b296ed4aab5bf02
Shail Shah <shailshah9@gmail.com>
2015-07-17 17:47:06 +0530

    conflict bug fixes

--- js/init.js
+++ js/init.js
@@ -2,11 +2,9 @@
   $(function(){
 
     $('.button-collapse').sideNav();
-<<<<<<< HEAD
     $('.parallax').parallax();
 	$('.slider').slider();
-=======
->>>>>>> origin/master
+
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
