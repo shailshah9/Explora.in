f011d45e51aa591699e162fd039d5d74b88eaf93
Shail Shah <shailshah9@gmail.com>
2015-07-18 00:15:47 +0530

    Reg form added, and some minor fixes

--- js/init.js
+++ js/init.js
@@ -1,8 +1,16 @@
 (function($){
   $(function(){
     $('.button-collapse').sideNav();
-	$('.slider').slider();
+	$('.slider').slider({height:500});
+	$('.modal-trigger').leanModal({
+	      dismissible: true, // Modal can be dismissed by clicking outside of the modal
+	      opacity: .8, // Opacity of modal background
+	      in_duration: 300, // Transition in duration
+	      out_duration: 200
+    	}
+  	);
   });
+ 
 })(jQuery);
 
 
