d47c0eaedf85b7a1daec73c77088d56f86be41a1
Shail Shah <shailshah9@gmail.com>
2015-09-17 22:21:27 +0530

    connection string

--- admin/connection.php
+++ admin/connection.php
@@ -1,6 +1,6 @@
 <?php
 
-//$connection = mysqli_connect("localhost","explora_new","random1234rewA","explora_new") or die("Error " . mysqli_error($connection));
+
 $connection = mysqli_connect("localhost","root","","explora") or die("Error " . mysqli_error($connection));
 
 ?>
